72189df061134fa3f9953c8e9d55e146ca211713
Soeren Wegener <wegener92@gmail.com>
2020-12-27 15:25:38 +0100

    Basic ui with dummy data

--- webserver/app.py
+++ webserver/app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from datetime import datetime, timedelta
 
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
 
 @app.route("/")
 def index():
-    return "Hello world"
+    games = [
+        {"title": "A vs B", "timestamp": datetime.now(), "id": 420},
+        {"title": "C vs B", "timestamp": datetime.now() - timedelta(minutes=3), "id": 520},
+        {"title": "C vs A", "timestamp": datetime.now() - timedelta(minutes=6), "id": 620},
+    ]
+    return render_template("index.html", games=games)
