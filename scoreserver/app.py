08bacb40ed184d5b96e1882f0c700ff1a5698e52
Soeren Wegener <wegener92@gmail.com>
2020-12-27 17:55:41 +0100

    Dummy login method in webserver. Checked in missing static files

--- scoreserver/app.py
+++ scoreserver/app.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 import asyncio
-import functools
 import logging
+import os
+from asyncio import sleep
 
+import redis as redis
 import requests
 import random
+import itertools
 
 logging.basicConfig(
     level=logging.INFO, format="[%(asctime)s] [%(levelname)s] %(name)s: %(message)s"
 )
 
-GAMESERVER_URL = "http://gameserver:5000/jsonrpc"
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
+GAMESERVER_HOST = os.environ.get("GAMESERVER_HOST", "gameserver")
+
+GAMESERVER_URL = f"http://{GAMESERVER_HOST}:5000/jsonrpc"
+
+db = redis.Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
 
 
 async def run_match(*bot_names):
@@ -30,7 +38,17 @@ async def run_match(*bot_names):
 
 
 async def main():
-    game_history = await run_match("soeren", "soerface")
+    while True:
+        users = db.keys("users:*")
+        for key_a, key_b in itertools.combinations(users, 2):
+            user_a, user_b = db.get(key_a), db.get(key_b)
+            logging.info(f"Next match: {user_a} vs {user_b}")
+            game_history = await run_match("soeren", "soerface")
+            logging.info("Result")
+            from pprint import pprint
+            pprint(game_history)
+        logging.info("Sleeping, next matches will start soon")
+        await sleep(10)
 
 
 if __name__ == '__main__':
--- webserver/app.py
+++ webserver/app.py
@@ -1,7 +1,10 @@
+import json
 import logging
+import os
+import random
 from datetime import datetime, timedelta
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import redis
 
 logging.basicConfig(
@@ -9,16 +12,23 @@ logging.basicConfig(
 )
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
-db = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+db = redis.Redis(host=os.environ.get("REDIS_HOST", "redis"), port=6379, db=0, decode_responses=True)
 
 
 @app.route("/")
 def index():
     game_keys = sorted(db.keys("games:*"))
-    games = [db.get(k) for k in game_keys]
+    games = [json.loads(db.get(k)) for k in game_keys]
     # games = [
     #     {"title": "A vs B", "timestamp": datetime.now(), "id": 420},
     #     {"title": "C vs B", "timestamp": datetime.now() - timedelta(minutes=3), "id": 520},
     #     {"title": "C vs A", "timestamp": datetime.now() - timedelta(minutes=6), "id": 620},
     # ]
     return render_template("index.html", games=games)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    user_id = random.randint(0, 10000)  # probably we get some kind of unique stuff from oauth flow
+    db.set(f"users:{user_id}", json.dumps({"botname": request.form.get("username")}))
+    return redirect("/")
