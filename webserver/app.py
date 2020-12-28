fcc30166df31067dcbce16524516cca58ad0f543
Soeren Wegener <wegener92@gmail.com>
2020-12-28 18:00:58 +0100

    Add rc3 oauth login route
    Co-authored-by: Lennart Bernhardt <l.bernhardt@live.de>

--- webserver/app.py
+++ webserver/app.py
@@ -5,6 +5,7 @@ import subprocess
 from urllib.parse import urlparse
 import re
 
+import requests
 from flask import Flask, render_template, request, redirect, jsonify, abort, url_for
 import redis
 
@@ -13,6 +14,12 @@ logging.basicConfig(
 )
 
 VALID_REPO_TEMPLATES = ["python"]
+
+RC3_CLIENT_ID = os.environ["RC3_CLIENT_ID"]
+RC3_REDIRECT_URI = os.environ["RC3_REDIRECT_URI"]
+RC3_TOKEN_URI = os.environ["RC3_TOKEN_URI"]
+RC3_CLIENT_SECRET = os.environ["RC3_CLIENT_SECRET"]
+AUTH_TOKEN_SECRET = os.environ["AUTH_TOKEN_SECRET"]
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 db = redis.Redis(host=os.environ.get("REDIS_HOST", "redis"), port=6379, db=0, decode_responses=True)
@@ -51,11 +58,46 @@ def login_failed():
     return render_template("login_failed.html")
 
 
-@app.route("/login", methods=["POST"])
-def login():
+@app.route("/login/local", methods=["POST"])
+def login_local():
     username = request.form.get("username")
     template = request.form.get("template")
     pubkey = request.form.get("pubkey")
+
+    if create_user(username, template, pubkey):
+        return redirect(url_for(".login_failed"))
+
+    return redirect(url_for(".login_success", username=username))
+
+
+@app.route("/login/rc3", methods=["POST"])
+def login_rc3():
+    if "code" not in request.args:
+        abort(400, "authorization code missing")
+    if "state" not in request.args:
+        abort(400, "state missing")
+
+    code = request.args["code"]
+    state = request.args["state"]
+    # TODO: what is that state?
+    stored_state = db.get(f"webserver:state:{state}")
+
+    username = "OAUTH"
+    template = VALID_REPO_TEMPLATES[0]  # user should be able to choose
+    pubkey = "ssh-rsa AAAAA"
+
+    resp = requests.post(
+        f"https://rc3.world/sso/token/"
+        f"?grant_type=authorization_code"
+        f"&code={code}"
+        f"&redirect_uri={RC3_TOKEN_URI}"
+        f"&client_id={RC3_CLIENT_ID}"
+        f"&client_secret={RC3_CLIENT_SECRET}")
+
+    resp_data = resp.json()
+    if resp_data.error:
+        logging.error("failed to exchange auth code for token: %s", resp_data)
+        abort(500)
 
     if create_user(username, template, pubkey):
         return redirect(url_for(".login_failed"))
