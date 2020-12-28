a0ae3138abfc2cc1d48d5573471c9e2253674ed8
thewavelength <thewavelength@users.noreply.github.com>
2020-12-28 19:19:13 +0100

    Remove example code

--- gitserver/bot-templates/c-sharp/Startup.cs
+++ gitserver/bot-templates/c-sharp/Startup.cs
@@ -12,14 +12,11 @@ namespace Wabooorrt
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddJsonRpc();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -28,19 +25,6 @@ namespace Wabooorrt
 			}
 
 			app.UseRouting();
-
-			// app.UseEndpoints(endpoints =>
-			// {
-			// 	endpoints.MapGet("/", async context =>
-			// 	{
-			// 		await context.Response.WriteAsync("Hello World!");
-			// 	});
-			// });
-
-			// app.UseJsonRpc(options =>
-			// {
-			// 	options.AddControllerWithCustomPath<BotController>("/");
-			// });
 
 			app.UseJsonRpc();
 		}
