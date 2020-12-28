412e0477666014c913194dcf02f6a72e13bc1e87
thewavelength <thewavelength@users.noreply.github.com>
2020-12-28 21:30:04 +0100

    Improve JSON-RPC test calls

--- gitserver/bot-templates/jsonrpc-test.sh
+++ gitserver/bot-templates/jsonrpc-test.sh
@@ -1,3 +1,3 @@
 curl -s -X POST -H "Content-Type: application/json" -d '{"method": "health", "jsonrpc": "2.0", "id": 420, "params": {}}' http://localhost:4000/jsonrpc | jq .result
 
-curl -s -X POST -H "Content-Type: application/json" -d '{"method": "next_action", "jsonrpc": "2.0", "id": 420, "params": {"game_state": "stuff", "your_name": "Hans"}}' http://localhost:4000/jsonrpc | jq .result
+curl -s -X POST -H "Content-Type: application/json" --data-binary "@jsonrpc-testinput.txt" http://localhost:4000/jsonrpc | jq .result
