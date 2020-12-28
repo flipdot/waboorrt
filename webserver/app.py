95f76cf034aa022d0cc000be80a029d034b7d209
Philipp Mandler <info@philipp-mandler.com>
2020-12-28 20:32:53 +0100

    webserver: /api/games route

--- webserver/app.py
+++ webserver/app.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 import re
 
 import requests
-from flask import Flask, render_template, request, redirect, jsonify, abort, url_for
+from flask import Flask, render_template, request, redirect, jsonify, abort, url_for, Response
 import redis
 
 logging.basicConfig(
@@ -28,23 +28,24 @@ db = redis.Redis(host=os.environ.get("REDIS_HOST", "redis"), port=6379, db=0, de
 @app.route("/")
 def index():
     game_keys = [f"game:{x}:summary" for x in db.zrevrangebyscore("matches_by_time", "+inf", "-inf", start=0, num=25)]
-    logging.debug(f"game_keys: {game_keys}")
-    # game_keys = sorted(db.keys("game:*:summary"))
     games = [json.loads(db.get(k)) for k in game_keys]
-    # games = [
-    #     {"title": "A vs B", "timestamp": datetime.now(), "id": 420},
-    #     {"title": "C vs B", "timestamp": datetime.now() - timedelta(minutes=3), "id": 520},
-    #     {"title": "C vs A", "timestamp": datetime.now() - timedelta(minutes=6), "id": 620},
-    # ]
     return render_template("index.html", games=games)
 
 
-@app.route("/game/<game_id>")
+@app.route("/api/games")
+def game_list():
+    game_keys = [f"game:{x}:summary" for x in db.zrevrangebyscore("matches_by_time", "+inf", "-inf", start=0, num=25)]
+    games = [json.loads(db.get(k)) for k in game_keys]
+    return jsonify(games)
+
+
+@app.route("/api/games/<game_id>")
 def game_history(game_id):
     history = db.get(f"game:{game_id}:history")
     if not history:
         abort(404)
-    return history
+
+    return Response(history, mimetype='application/json')
 
 
 @app.route("/login_success/<username>")
