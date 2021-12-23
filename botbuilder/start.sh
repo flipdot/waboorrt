56696d27a73d83a0eaa6b6a54876099f62f64fd9
Philipp Mandler <info@philipp-mandler.com>
2021-12-23 01:47:30 +0100

    botbuilder: Read queue from redis

--- botbuilder/start.sh
+++ botbuilder/start.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 while true; do
-  inotifywait -e create /botbuilder;
-  while [ -n "$(ls /botbuilder/)" ]; do
-    file=$(find /botbuilder/ -type f -exec basename {} \; | head -n1);
-    imageName=$(printf %s "$file" | sha1sum | head -c 32)
+  botname="$(redis-cli -h redis BZPOPMIN 'botbuilder:queue' 0 | head -n 2 | tail -n 1)"
+  imageName=$(printf %s "$file" | sha1sum | head -c 32)
 
-    TMP_DIR=$(mktemp -d)
-    cd "$TMP_DIR" || exit
-    echo "Building bot $file"
-    git clone "/git/$file.git" .
-    docker build -t "localhost/bot/$imageName" .
-    redis-cli -h redis set "user:$file" "{\"botname\": \"$file\"}"
-    cd "$HOME" || exit
-
-    rm "/botbuilder/$file";
-    done;
+  TMP_DIR=$(mktemp -d)
+  cd "$TMP_DIR" || exit
+  echo "Building bot $botname"
+  git clone "ssh://git@gitserver:2222/$botname.git" .
+  docker build -t "localhost/bot/$imageName" .
+  redis-cli -h redis set "user:$botname" "{\"botname\": \"$botname\"}"
+  cd "$HOME" || exit
 done
