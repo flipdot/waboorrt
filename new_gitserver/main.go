ee5c3685a8255d3489eda03b7473a19a73c71b50
Soeren Wegener <wegener92@gmail.com>
2021-12-23 23:55:27 +0100

    [gitserver] Get user id from webserver. Perform login and save session token

--- new_gitserver/main.go
+++ new_gitserver/main.go
@@ -5,11 +5,26 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/sosedoff/gitkit"
+	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/sosedoff/gitkit"
+	"net/url"
+	"os"
 )
+
+// Defaults suitable for local development if webserver is started via compose
+var WEBSERVER_HOST = getenv("WEBSERVER_HOST", "localhost")
+var WEBSERVER_PORT = getenv("WEBSERVER_PORT", "80")
+var sessionToken string
+
+func getenv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
 
 type checkPublicKeyRequest struct {
 	PublicKey string `json:"publicKey"`
@@ -23,19 +38,61 @@ func httpSuccess(statusCode int) bool {
 	return statusCode >= 200 && statusCode <= 299
 }
 
+func endpointUrl(endpoint string) string {
+	var baseUrl string
+	if WEBSERVER_PORT == "443" {
+		baseUrl = "https://" + WEBSERVER_HOST
+	} else if WEBSERVER_PORT == "80" {
+		baseUrl = "http://" + WEBSERVER_HOST
+	} else {
+		baseUrl = "http://" + WEBSERVER_HOST + ":" + WEBSERVER_PORT
+	}
+	return baseUrl + "/api" + endpoint
+}
+
+func getSessionToken() (string, error) {
+	reqBody, err := json.Marshal(&loginRequest{Username: "gitserver"})
+	if err != nil {
+		return "", err
+	}
+	// TODO: this endpoint is currently only available for local development since it doesn't check credentials
+	resp, err := http.Post(endpointUrl("/auth/login"), "application/json", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return "", err
+	}
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(string(resBody))
+	return string(resBody), nil
+}
+
 // User-defined key lookup function. You can make a call to a database or
 // some sort of cache storage (redis/memcached) to speed things up.
 // Content is a string containing ssh public key of a user.
 func lookupKey(content string) (*gitkit.PublicKey, error) {
-	body, err := json.Marshal(&checkPublicKeyRequest{PublicKey: content})
-	if err != nil {
-		return nil, err
+	httpClient := &http.Client{}
+	if sessionToken == "" {
+		var err error
+		sessionToken, err = getSessionToken()
+		if err != nil {
+			return nil, err
+		}
 	}
-	resp, err := http.Post("http://webserver/git/check-public-key", "application/json", bytes.NewBuffer(body))
+	apiUrl := endpointUrl("/internal/users/by-ssh-key/" + url.PathEscape(content))
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	req.Header.Add("X-Session-Key", sessionToken)
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusForbidden {
+		sessionToken = ""
+		return nil, errors.New("API returned 403 forbidden. Session expired? Try again")
+	}
 
 	if resp.StatusCode == http.StatusNotFound {
 		return nil, errors.New(fmt.Sprintf("public key not registered: %s", content))
@@ -45,18 +102,20 @@ func lookupKey(content string) (*gitkit.PublicKey, error) {
 		return nil, errors.New(fmt.Sprintf("Checking public key failed with status code %d", resp.StatusCode))
 	}
 
-	var respBody []byte
-	_, err = resp.Body.Read(respBody)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	responseData := checkPublicKeyResponse{}
-	err = json.Unmarshal(respBody, &responseData)
+	err = json.Unmarshal(body, &responseData)
 	if err != nil {
 		return nil, err
 	}
-
 	return &gitkit.PublicKey{Id: responseData.Id}, nil
+}
+
+type loginRequest struct {
+	Username string `json:"username"`
 }
 
 type checkAuthorizationRequest struct {
@@ -73,7 +132,8 @@ func authorize(keyId string, repo string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	resp, err := http.Post("http://webserver/git/check-access", "application/json", bytes.NewBuffer(body))
+
+	resp, err := http.Post(endpointUrl("/internal/user/by-ssh-key/"), "application/json", bytes.NewBuffer(body))
 	if err != nil {
 		return false, err
 	}
@@ -115,6 +175,7 @@ func main() {
 	server.Authorize = authorize
 
 	// Specify host and port to run the server on.
+	fmt.Println("Git / SSH server listening on localhost:2222")
 	err := server.ListenAndServe(":2222")
 	if err != nil {
 		log.Fatal(err)
