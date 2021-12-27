9862fea939b3bc42e40cc35972c4ec18f3ce1cfc
Soeren Wegener <wegener92@gmail.com>
2021-12-27 22:04:39 +0100

    Revert "[webserver] Case-insensitive unique usernames"
    This reverts commit 4d248dfe17ab0008c59f5feb5911da066aa04716.

--- webserver/models.py
+++ webserver/models.py
@@ -2,8 +2,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from uuid import uuid4
-from sqlalchemy.sql.functions import func
-from sqlalchemy.sql.schema import Index
 
 from database import Base
 
@@ -11,13 +9,11 @@ from database import Base
 class UserModel(Base):
     __tablename__ = "users"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, index=True, unique=True)
-    username = Column(String, index=True)
+    username = Column(String, index=True, unique=True)
     rc3_identity = Column(String, index=True, unique=True, nullable=True)
     ssh_public_key = Column(String, unique=True)
     repository = relationship("RepositoryModel", back_populates="owner", uselist=False)
     is_superuser = Column(Boolean, default=False)
-
-Index('user_username_index', func.lower(UserModel.username), unique=True)
 
 
 class RepositoryModel(Base):
