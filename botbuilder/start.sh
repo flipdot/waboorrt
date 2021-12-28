c8dd79ce3c96f19d03eb62c244817526fa549ead
Philipp Mandler <info@philipp-mandler.com>
2021-12-28 15:21:30 +0100

    [botbuilder]: Use ed25519

--- botbuilder/start.sh
+++ botbuilder/start.sh
@@ -5,11 +5,11 @@ mkdir -p /root/.ssh
 
 ssh-keyscan -p $GITSERVER_PORT $GITSERVER_HOST > /root/.ssh/known_hosts
 
-if [ ! -f /root/.ssh/id_rsa ]; then
-  ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+if [ ! -f /root/.ssh/id_ed25519 ]; then
+  ssh-keygen -b 2048 -t ed25519 -f /root/.ssh/id_ed25519 -q -N ""
 fi
 
-SSH_PUBLIC_KEY=$(cat /root/.ssh/id_rsa.pub)
+SSH_PUBLIC_KEY=$(cat /root/.ssh/id_ed25519.pub)
 
 echo "Copying SSH key to http://$WEBSERVER_HOST/api/internal/users"
 curl -s -X POST "http://$WEBSERVER_HOST/api/internal/users" \
@@ -22,7 +22,7 @@ build () {
 
   TMP_DIR=$(mktemp -d)
   cd "$TMP_DIR"
-  echo "Building bot $1"
+  echo "Building bot $1" >&2
   git clone "ssh://git@$GITSERVER_HOST:$GITSERVER_PORT/$1" . || return 1
   docker build -t "localhost/bot/$imageName" . || return 1
   redis-cli -h redis set "user:$1" "{\"botname\": \"$1\"}"
@@ -31,6 +31,5 @@ build () {
 
 while true; do
   botname="$(redis-cli -h redis BZPOPMIN 'botbuilder:queue' 0 | head -n 2 | tail -n 1)"
-  echo "Building bot $botname"
   build $botname || echo "Warning: Failed to build bot $botname"
 done
